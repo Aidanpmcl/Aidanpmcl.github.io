27fdf96d04f332ea2c069de2a1f03a2d8a606885
Aidanpmcl <aimc1809@colorado.edu>
2024-04-16 10:47:55 -0600

    yay

---
+++ wa/wa13/wa13.js
@@ -0,0 +1,114 @@
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+      }
+    
+      collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+                const dx = ball.x - this.x;
+                const dy = ball.y - this.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+    
+                if (distance < this.size + ball.size) {
+                    const unitX = dx / distance;
+                    const unitY = dy / distance;
+                    const v1x = this.velX;
+                    const v1y = this.velY;
+                    const v2x = ball.velX;
+                    const v2y = ball.velY;
+                    const parallelVel1 = v1x * unitX + v1y * unitY;
+                    const parallelVel2 = v2x * unitX + v2y * unitY;
+    
+                    this.velX -= 2 * parallelVel1 * unitX;
+                    this.velY -= 2 * parallelVel1 * unitY;
+                    ball.velX -= 2 * parallelVel2 * unitX;
+                    ball.velY -= 2 * parallelVel2 * unitY;
+                }
+            }
+        }
+    }
+}    
+
+const balls = [];
+
+while (balls.length < 25) {
+    const size = random(1,20)
+    const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-10, 10),
+        random(-10, 10),
+        randomRGB(),
+        size,
+    )
+
+    balls.push(ball);
+    }
+
+function loop () {
+    ctx.fillStyle = "rgb( 0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
+    requestAnimationFrame(loop);
+}
+
+loop();
